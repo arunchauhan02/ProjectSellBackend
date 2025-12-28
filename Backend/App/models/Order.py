9b79ac5e1ec132bbbbc7ffae096b45fc1708725c
arunchauhan02 <100798589+arunchauhan02@users.noreply.github.com>
2025-12-29 00:42:09 +0530

    Add Order model with payment details

--- Backend/App/models/Order.py
+++ Backend/App/models/Order.py
@@ -1,1 +1,8 @@
-
+class Order(BaseModel):
+    id: Optional[str]
+    user_id: str
+    project_id: str
+    amount_paid: float
+    payment_status: str
+    payment_id: Optional[str]
+    purchased_at: datetime= datetime.utcnow()
